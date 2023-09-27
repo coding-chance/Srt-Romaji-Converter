76b40e4b665a834d9fa7583c56fd4ca6b9c0ee1a
coding-chance <90079117+coding-chance@users.noreply.github.com>
2023-09-27 22:18:19 +0200

    Show conversion results on terminal

--- srt-romaji-converter.py
+++ srt-romaji-converter.py
@@ -1,4 +1,3 @@
-import pykakasi
 import pysrt
 import os
 import sys
@@ -16,7 +15,6 @@ for file in os.listdir(input_dir):
     if not file.startswith('.') and os.path.isfile(os.path.join(input_dir, file)):
         input_srtname = file
         # print(input_srtname)
-        
 
 
 # Extract text data from srt file
@@ -32,7 +30,6 @@ subtitle_lines = f.readlines()
 # Convert jp_texts to romaji
 romaji_converter = cutlet.Cutlet()
 romaji_converter.use_foreign_spelling = False
-# kks = pykakasi.kakasi()
 
 romaji_wordlist = []
 for jp_word in jp_texts:
@@ -50,22 +47,27 @@ for phonetic in romaji_wordlist:
         replaced_phonetic = phonetic.replace("i", "ï").replace("e", "é").replace("ch", "tch").replace("r", "l")
     complete_phonetics.append(replaced_phonetic)
 
-# print(f"complete_phonetics: {complete_phonetics}")
+# Display original sentences and romaji
+for index, phonetic in enumerate(complete_phonetics):
+    print(f"{jp_texts[index]}")
+    print(f"{phonetic}")
+    print()
+    
 
+# Put together original text and romaji as srt format
 outputs = []
-
 for index, line in enumerate(subtitle_lines):
-    print(f"len(): {len(complete_phonetics)}")
     if index == 2 or index % 4 == 2 and index > 2:
-        print(f"index: {index} ( {line} was appended to complete_phonetics )")
         outputs.append(f"{complete_phonetics[0]}\n")
         complete_phonetics.pop(0)
     else:
         outputs.append(line)
+
     # if there's no element in complete_phonetics, break the for loop
     if len(complete_phonetics) == 0:
         break
 f.close()
+
 
 # Save file as srt
 with open(f'output/romaji-{input_srtname.split(".")[0]}-{formatted_time}.srt', 'x') as f:
